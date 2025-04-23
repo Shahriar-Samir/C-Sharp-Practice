22d968328c56625995986881c613a95c77d9b2e7
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-04-23 15:32:08 +0600

    added: practice2

---
+++ practice-2/Program.cs
@@ -0,0 +1,10 @@
+﻿static double Addition(double a,double b){
+    return a+b;
+}
+
+Console.WriteLine("A = ?");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("B = ?");
+double b= Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Addition(a,b));
