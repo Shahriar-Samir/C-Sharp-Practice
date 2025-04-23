c0757843c6cd139e9a036732dd1720dabc894fa7
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-04-23 14:47:05 +0600

    added: practice1

---
+++ practice-1/Program.cs
@@ -0,0 +1,12 @@
+﻿string sentence = "This is good";
+char oneChar = '*';
+bool isOkay = true;
+int age = 30;
+double percentage = 56.78;
+
+Console.WriteLine(sentence);
+Console.WriteLine("Your age is "+ age);
+Console.WriteLine("Work done"+percentage);
+Console.WriteLine("He is okay and that is "+isOkay+" "+isOkay);
+
+
